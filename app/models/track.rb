e458f58e4ffe4c88ca475c262bd47e340453c5a6
Lena <fatmeow@gmail.com>
2017-11-24 10:52:01 -0500

    Add scope methods

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,25 +3,34 @@ class Track < ApplicationRecord
   SHORT = 180_000
   LONG = 360_000
 
-  def self.short
-    shorter_than(SHORT)
-  end
+  scope :short, -> { shorter_than(SHORT) }
+  scope :medium, -> { longer_or_equal_to(SHORT).shorter_than(LONG) }
+  scope :long, -> { longer_or_equal_to(LONG) }
+  scope :starts_with, -> (char) { where('name ILIKE ?', "#{ char }%") }
 
-  def self.medium
-    longer_or_equal_to(SHORT).shorter_than(LONG)
-  end
+  scope :shorter_than, -> (milliseconds) {
+    where("milliseconds < ?", milliseconds) if milliseconds && milliseconds > 0
+  }
 
-  def self.long
-    longer_or_equal_to(LONG)
-  end
+  # def self.short
+  #   shorter_than(SHORT)
+  # end
 
-  def self.shorter_than(milliseconds)
-    if milliseconds && milliseconds > 0
-      where("milliseconds < ?", milliseconds)
-    else
-      all
-    end
-  end
+  # def self.medium
+  #   longer_or_equal_to(SHORT).shorter_than(LONG)
+  # end
+
+  # def self.long
+  #   longer_or_equal_to(LONG)
+  # end
+
+  # def self.shorter_than(milliseconds)
+  #   if milliseconds && milliseconds > 0
+  #     where("milliseconds < ?", milliseconds)
+  #   else
+  #     all
+  #   end
+  # end
 
   def self.longer_or_equal_to(milliseconds)
     if milliseconds && milliseconds > 0
@@ -31,8 +40,8 @@ class Track < ApplicationRecord
     end
   end
 
-  def self.starts_with(char)
-    where('name ILIKE ?', "#{ char }%")
-  end
+  # def self.starts_with(char)
+  #   where('name ILIKE ?', "#{ char }%")
+  # end
 
 end
