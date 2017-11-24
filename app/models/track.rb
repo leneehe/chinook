1405ce655638b3ddaeef754226386da89b369ed5
Lena <fatmeow@gmail.com>
2017-11-24 10:26:37 -0500

    Add class methods to check track playtime

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,26 @@
 class Track < ApplicationRecord
+
+  SHORT = 180_000
+  LONG = 360_000
+
+  def self.short
+    shorter_than(SHORT)
+  end
+
+  def self.medium
+    longer_or_equal_to(SHORT).shorter_than(LONG)
+  end
+
+  def self.long
+    longer_or_equal_to(LONG)
+  end
+
+  def self.shorter_than(milliseconds)
+    Track.where("milliseconds < ?", milliseconds)
+  end
+
+  def self.longer_or_equal_to(milliseconds)
+    Track.where("milliseconds > ?", milliseconds)
+  end
+
 end
